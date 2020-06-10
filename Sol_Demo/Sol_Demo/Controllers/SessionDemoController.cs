09488eb6d71c42c95fd8194e27c6101f2bfa66a0
Kishor Naik <kishor.naik011.net@outlook.com>
2020-06-10 13:19:42 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/SessionDemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Demo.Controllers
+{
+    public class SessionDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("MySession", "Hello Asp.net Core MVC");
+
+            return View();
+        }
+
+        public IActionResult GetSessionValue()
+        {
+            ViewBag.SessionValue = HttpContext.Session.GetString("MySession");
+
+            HttpContext.Session.Remove("MySession");
+
+            return View("Index");
+        }
+    }
+}
